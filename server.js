7ee3912abfa9bb8824ea93527d1f56c216fed2bd
yu0819ki <yu0819ki@gmail.com>
2022-03-07 11:57:27 +0900

    add minimal router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const Koa = require('koa');
+const router = require('koa-path-match')();
 const app = new Koa();
 
 // Constants
@@ -7,9 +8,10 @@ const SERVER_PORT = _.get(process.env, 'PORT', _.get(process.env, 'SERVER_PORT',
 const SERVER_HOST = _.get(process.env, 'HOST', _.get(process.env, 'SERVER_HOST', 80));
 
 // response
-app.use(ctx => {
+app.use(router('/')
+  .get(async ctx => {
     ctx.body = 'Hello Koa';
-});
+  }));
 
 app.listen(SERVER_PORT, SERVER_HOST);
 console.log(`Running on http://${SERVER_HOST}:${SERVER_PORT}`);
