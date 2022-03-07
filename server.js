aeddf698b1a32432fe2e292097ca64c015edc48f
yu0819ki <yu0819ki@gmail.com>
2022-03-07 13:30:30 +0900

    implement `unfurl` based OGP Parser API

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const createError = require('http-errors');
 const _ = require('lodash');
+const qs = require('qs');
+const { unfurl } = require('unfurl.js');
+const { FetchError } = require('node-fetch');
 const Koa = require('koa');
 const router = require('koa-path-match')();
 const app = new Koa();
@@ -11,6 +15,25 @@ const SERVER_HOST = _.get(process.env, 'HOST', _.get(process.env, 'SERVER_HOST',
 app.use(router('/')
   .get(async ctx => {
     ctx.body = 'Hello Koa';
+  }));
+app.use(router('/parse')
+  .get(async ctx => {
+    const url = _.get(qs.parse(ctx.request.querystring), 'url');
+    if (!url) {
+      throw createError(400);
+    }
+    try {
+      ctx.body = await unfurl(url, {
+        oembed: true,
+        timeout: 300000,
+        userAgent: 'facebookexternalhit;ogp-tools-parser/0.1',
+      });
+    } catch (e) {
+      if (e instanceof FetchError && e.type === 'request-timeout') {
+        throw createError(504);
+      }
+      throw e;
+    }
   }));
 
 app.listen(SERVER_PORT, SERVER_HOST);
